d59591f86d635616105df57287fcc6e6abd0294d
Sydney Bär <baer.sydney@outlook.com>
2023-10-13 12:40:25 +0200

    added background-colors

--- script.js
+++ script.js
@@ -5,51 +5,38 @@ let response;
 let getPokemonName;
 let getIdOfPokemon;
 let getImgOfPokemon;
+let getFirstTypeOfPokemon;
+// let getSecondTypeOfPokemon;
 
+
+
+let fighting = 'fighting';
+let psychic = 'psychic';
+let poison = 'poison';
+let dragon = 'dragon';
+let ghost = 'ghost';
+let dark = 'dark';
+let ground = 'ground';
+let fire = 'fire';
+let fairy = 'fairy';
+let water = 'water';
+let flying = 'flying';
+let normal = 'normal';
+let rock = 'rock';
+let electric = 'electric';
+let bug = 'bug';
+let grass = 'grass';
+let ice = 'ice';
+let steel = 'steel';
 
 /* Declare arrays */
 let currentPokemon = [];
-let pokemonNames = [
-    "Bulbasaur",
-    "Ivysaur",
-    "Venusaur",
-    "Charmander",
-    "Charmeleon",
-    "Charizard",
-    "Squirtle",
-    "Wartortle",
-    "Blastoise",
-    "Caterpie",
-    "Metapod",
-    "Butterfree",
-    "Weedle",
-    "Kakuna",
-    "Beedrill",
-    "Pidgey",
-    "Pidgeotto",
-    "Pidgeot",
-    "Rattata",
-    "Raticate",
-    "Spearow",
-    "Fearow",
-    "Ekans",
-    "Arbok",
-    "Pikachu",
-    "Raichu",
-    "Sandshrew",
-    "Sandslash",
-    "Nidorina",
-    "Nidoqueen",
-    "Nidorino",
-    "Nidoking",
-    "Clefairy",
-    "Clefable",
-    "Vulpix",
-    "Ninetales"
-]; // Auslagern
+let pokemonNames = ["Bulbasaur", "Ivysaur", "Venusaur", "Charmander", "Charmeleon", "Charizard", "Squirtle", "Wartortle", "Blastoise", "Caterpie", "Metapod", "Butterfree", "Weedle", "Kakuna", "Beedrill", "Pidgey", "Pidgeotto", "Pidgeot", "Rattata", "Raticate", "Spearow", "Fearow", "Ekans", "Arbok", "Pikachu", "Raichu", "Sandshrew", "Sandslash", "Nidorina", "Nidoqueen", "Nidorino", "Nidoking", "Clefairy", "Clefable", "Vulpix", "Ninetales"];
+// Auslagern
 let pokemonId = [];
 let pokemonImg = [];
 let cardBackgroundColor = [];
+let compareBackgroundColor = ['fighting', 'psychic', 'poison', 'dragon', 'ghost', 'dark', 'ground', 'fire', 'fairy', 'water', 'flying', 'normal', 'rock', 'electric', 'bug', 'grass', 'ice', 'steel'];
 
 function init() {
     includeHTML();
@@ -70,36 +57,64 @@ async function loadPokemon() {
         getPokemonName = currentPokemon['name'];
         getIdOfPokemon = currentPokemon['id'];
         getImgOfPokemon = currentPokemon['sprites']['front_shiny'];
+        getFirstTypeOfPokemon = currentPokemon['types'][0]['type']['name'];
+        // getSecondTypeOfPokemon = currentPokemon['types'][1]['type']['name'];
+        console.log(getFirstTypeOfPokemon);
+        // console.log(getSecondTypeOfPokemon);
         pokemonId.push(getIdOfPokemon);
         pokemonImg.push(getImgOfPokemon);
-        cardBackgroundColor.push(currentPokemon['game_indices'][1]['version']['name']);
+        cardBackgroundColor.push(getFirstTypeOfPokemon);
         console.log('Loaded Pokémon', currentPokemon);
         renderPokemonCards(i);
     }
-
-    // renderPokemonInfo(currentPokemon);
 }
 
-// function renderPokemonInfo(currentPokemon) {
-//     let name = currentPokemon['name'];
-//     let imgPath = currentPokemon['sprites']['front_shiny'];
-//     document.getElementById('pokemonName').innerHTML = `${name}`;
-//     document.getElementById('pokemonImage').src = `${imgPath}`;
-// }
-
-/* Render pokemon card */
+/* Get pokedex container to render in all pokemon cards */
 function renderPokemonCards(i) {
     let renderPokemonCards = document.getElementById('pokedex');
-    renderPokemonCards.innerHTML += /*html*/`
-       <div id="card${i}" class="poke-card">
-           <div id="IdOfPokemon" class="p-around-8">#${pokemonId[i]}</div>
-           <div id="nameOfPokemon">${pokemonNames[i].toUpperCase()}</div>
-            <div class="flex space-betw">
-                <div>
-                    <div></div>
-                    <div></div>
-                </div>
-                <div id="imgPokemon"><img src="${pokemonImg[i]}" alt="pokemon"></div>
-            </div>
-       </div>`;
+    let typeOfPokemon = getFirstTypeOfPokemon[0].toUpperCase() + getFirstTypeOfPokemon.slice(1);
+    renderPokemonCards.innerHTML += renderCard(i, typeOfPokemon);
+    setBackgroundColor(i);
 }
+
+/* Render pokemon card content */
+function renderCard(i, typeOfPokemon) {
+    return /*html*/`
+    <div id="card${i}" class="poke-card">
+        <div id="IdOfPokemon" class="p-around-8">#${pokemonId[i]}</div>
+        <div id="nameOfPokemon">${pokemonNames[i].toUpperCase()}</div>
+         <div class="flex space-betw">
+             <div>
+                 <div id="pokemonType1">${typeOfPokemon}</div>
+                 <div id="pokemonType2"></div>
+             </div>
+             <div id="imgPokemon"><img src="${pokemonImg[i]}" alt="pokemon"></div>
+         </div>
+    </div>`;
+}
+
+/* Set background-color of each pokemon card */
+function setBackgroundColor(i) {
+    let getBackgroundColor = cardBackgroundColor[i];
+    if (getBackgroundColor === fighting) { document.getElementById(`card${i}`).classList.add('bgc-fighting'); }
+    else if (getBackgroundColor === psychic) { document.getElementById(`card${i}`).classList.add('bgc-psychic'); }
+    else if (getBackgroundColor === poison) { document.getElementById(`card${i}`).classList.add('bgc-poison'); }
+    else if (getBackgroundColor === dragon) { document.getElementById(`card${i}`).classList.add('bgc-dragon'); }
+    else if (getBackgroundColor === ghost) { document.getElementById(`card${i}`).classList.add('bgc-ghost'); }
+    else if (getBackgroundColor === dark) { document.getElementById(`card${i}`).classList.add('bgc-dark'); }
+    else if (getBackgroundColor === ground) { document.getElementById(`card${i}`).classList.add('bgc-ground'); }
+    else if (getBackgroundColor === fire) { document.getElementById(`card${i}`).classList.add('bgc-fire'); }
+    else if (getBackgroundColor === fairy) { document.getElementById(`card${i}`).classList.add('bgc-fairy'); }
+    else if (getBackgroundColor === water) { document.getElementById(`card${i}`).classList.add('bgc-water'); }
+    else if (getBackgroundColor === flying) { document.getElementById(`card${i}`).classList.add('bgc-flying'); }
+    else if (getBackgroundColor === normal) { document.getElementById(`card${i}`).classList.add('bgc-normal'); }
+    else if (getBackgroundColor === rock) { document.getElementById(`card${i}`).classList.add('bgc-rock'); }
+    else if (getBackgroundColor === electric) { document.getElementById(`card${i}`).classList.add('bgc-electric'); }
+    else if (getBackgroundColor === bug) { document.getElementById(`card${i}`).classList.add('bgc-bug'); }
+    else if (getBackgroundColor === grass) { document.getElementById(`card${i}`).classList.add('bgc-grass'); }
+    else if (getBackgroundColor === ice) { document.getElementById(`card${i}`).classList.add('bgc-ice'); }
+    else if (getBackgroundColor === steel) { document.getElementById(`card${i}`).classList.add('bgc-steel'); }
+    else { document.getElementById(`card${i}`).classList.add('bgc-default'); }
+}
+
+
